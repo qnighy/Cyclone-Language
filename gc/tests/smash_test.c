2cf660492a5effca72e873c6fc41d1415a24a4d2
Masaki Hara <ackie.h.gmai@gmail.com>
2022-07-23 12:39:32 +0900

    bdwgc 7.1 -> 8.0.6

--- gc/tests/smash_test.c
+++ gc/tests/smash_test.c
@@ -1,8 +1,11 @@
-/*
- * Test that overwrite error detection works reasonably.
- */
-#define GC_DEBUG
-#include <gc.h>
+/* Test that overwrite error detection works reasonably.        */
+
+#ifndef GC_DEBUG
+# define GC_DEBUG
+#endif
+
+#include "gc.h"
+
 #include <stdio.h>
 
 #define COUNT 7000
@@ -10,19 +13,29 @@
 
 char * A[COUNT];
 
-int main ()
+char * volatile q;
+
+int main(void)
 {
-        int i;
-	char *p;
+  int i;
+  char *p;
 
-	GC_INIT();
+  GC_INIT();
 
-        for (i = 0; i < COUNT; ++i) {
-		A[i] = p = GC_MALLOC(SIZE);
+  for (i = 0; i < COUNT; ++i) {
+     A[i] = p = (char*)GC_MALLOC(SIZE);
 
-		if (i%3000 == 0) GC_gcollect();
-		if (i%5678 == 0) p[SIZE + i/2000] = 42;
-	}
-	return 0;
+     if (i%3000 == 0) {
+        q = NULL;
+        GC_gcollect();
+     } else if (i%5678 == 0 && p != 0) {
+        /* Write a byte past the end of the allocated object    */
+        /* but not beyond the last word of the object's memory. */
+        /* A volatile intermediate pointer variable is used to  */
+        /* avoid a compiler complain of out-of-bounds access.   */
+        q = &p[(SIZE + i/2000) /* 42 */];
+        *q = 42;
+     }
+  }
+  return 0;
 }
-
